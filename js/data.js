5316bdcf78b4a364982e134de480a48e2be7b39d
linaLeinartaite <lleinartaite@yahoo.com>
2020-01-09 14:22:13 +0200

    myResume first commit

--- js/data.js
+++ js/data.js
@@ -1,2 +1,39 @@
 "use strict";
 
+const resume = [
+    {
+         //KAIP DARYT JEI exp ILGESNIS??? 
+         //KAIP DARYTI jei yra daugiau darbu?? (DAbar skeliu per puse i du stulpelius)
+         //ar imanoma palyginti stulpeliu aukscius??
+        company: "aplle",
+        dates: "OCT 2018 - present",
+        exp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque."
+    },
+    {
+        company: "google",
+        dates: "JUNE 2015 - OCT 2018",
+        exp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque."
+    },
+    {
+        company: "spotify",
+        dates: "DEC 2014 - JUNE 2015",
+        exp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque. "
+    },
+    {
+        company: "ibm",
+        dates: "NOW 2010 - DEC 2014",
+        exp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque."
+    }
+    // ,
+    // {
+    //     company: "ibm",
+    //     dates: "NOW 2010 - DEC 2014",
+    //     exp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque."
+    // }
+    // ,
+    // {
+    //     company: "ibm",
+    //     dates: "NOW 2010 - DEC 2014",
+    //     exp: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean tincidunt sit amet erat malesuada interdum. Aenean sodales dui quis leo fermentum scelerisque."
+    // }
+]
